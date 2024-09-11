57fb06ee66bd29fa04f8cd59bec347ac7fd3c000
Zane Fink <finkzane@gmail.com>
2024-09-11 17:34:19 -0500

    Update driver and config

--- examples/binomial_options/bo.py
+++ examples/binomial_options/bo.py
@@ -1,20 +1,26 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from nnueehcs.model_builder import EnsembleModelBuilder, KDEModelBuilder, DeltaUQMLPModelBuilder
-from nnueehcs.training import Trainer
+from nnueehcs.model_builder import EnsembleModelBuilder, KDEModelBuilder, DeltaUQMLPModelBuilder, PAGERModelBuilder
+from nnueehcs.training import Trainer, ModelSavingCallback
 from nnueehcs.data_utils import get_dataset_from_config
+from nnueehcs.evaluation import get_uncertainty_evaluator
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+import pytorch_lightning as L
 import yaml
 import click
 import matplotlib.pyplot as plt
 
+
 def get_trainer(trainer_config, name, model):
-    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0.00, patience=10, verbose=False, mode='min')]
+    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0.00, patience=300, verbose=False, mode='min'),
+                 ModelSavingCallback(monitor='val_loss')]
     extra_cbs = model.get_callbacks()
     if extra_cbs:
         callbacks.extend(extra_cbs)
     return Trainer(name, trainer_config, callbacks=callbacks)
+
 
 
 @click.command()
@@ -31,10 +37,9 @@ def main(eval_only):
     if eval_only is False:
         name = 'binomial_options'
 
-        # builder = EnsembleModelBuilder(model_cfg['architecture'], model_cfg['ensemble_model'])
 
         dset = get_dataset_from_config(dataset_cfg, 'bo_ds')
-        builder = DeltaUQMLPModelBuilder(model_cfg['architecture'], model_cfg['delta_uq'])
+        builder = KDEModelBuilder(model_cfg['architecture'], model_cfg['kde'])
         model = builder.build().to(dset.dtype)
         trainer = get_trainer(trainer_cfg, name, model)
 
@@ -47,16 +52,12 @@ def main(eval_only):
         ipt = (ipt - ipt.min()) / (ipt.max() - ipt.min())
         dset.input = ipt
 
-
-
-        split_data = dset.train_test_split(test_proportion=training_cfg['validation_split'])
-        train, test = split_data
-        train_dl = DataLoader(train, batch_size=training_cfg['batch_size'], shuffle=True)
-        test_dl = DataLoader(test, batch_size=training_cfg['batch_size'], shuffle=False)
+        train_dl = DataLoader(dset, batch_size=training_cfg['batch_size'], shuffle=True)
+        test_dl = DataLoader(dset, batch_size=training_cfg['batch_size'], shuffle=False)
         trainer.fit(model, train_dl, test_dl)
         torch.save(model, 'model.pth')
 
-    model = torch.load('model.pth')
+    model = torch.load('logs/binomial_options/version_0/model.pth')
 
     model.eval()
     with torch.no_grad():
@@ -66,37 +67,47 @@ def main(eval_only):
         id_ipt = dset_id.input.to(model.device)
         id_opt = dset_id.output.to(model.device)
 
-        id_ipt = (id_ipt - id_ipt.min()) / (id_ipt.max() - id_ipt.min())
-        id_opt = (id_opt - id_opt.min()) / (id_opt.max() - id_opt.min())
-
         ood_ipt = dset_ood.input.to(model.device)
         ood_opt = dset_ood.output.to(model.device)
 
+        # We need to normalize opt before normalizing ipt
         ood_ipt = (ood_ipt - id_ipt.min()) / (id_ipt.max() - id_ipt.min())
         ood_opt = (ood_opt - id_opt.min()) / (id_opt.max() - id_opt.min())
-        id_ipt = id_ipt[0:20000]
-        id_opt = id_opt[0:20000]
-        ood_ipt = ood_ipt[0:20000]
-        ood_opt = ood_opt[0:20000]
-        id_preds, id_ue = model(id_ipt, return_ue=True)
-        ood_preds, ood_ue = model(ood_ipt, return_ue=True)
-        
-        id_loss = nn.L1Loss()(id_preds, id_opt)
-        ood_loss = nn.L1Loss()(ood_preds, ood_opt)
-        print(f'ID Loss: {id_loss}')
-        print(f'OOD Loss: {ood_loss}')
 
-        print(f'Average ID uncertainty: {id_ue.mean()}')
-        print(f'Average OOD uncertainty: {ood_ue.mean()}')
-        from scipy.stats import wasserstein_distance
-        print(wasserstein_distance(id_ue.flatten(), ood_ue.flatten()))
+        id_ipt = (id_ipt - id_ipt.min()) / (id_ipt.max() - id_ipt.min())
+        id_opt = (id_opt - id_opt.min()) / (id_opt.max() - id_opt.min())
+
+        indices = torch.randperm(id_ipt.size(0))
+        indices_ood = torch.randperm(ood_ipt.size(0))
+        
+        id_ipt = id_ipt[indices][:20000]
+        id_opt = id_opt[indices][:20000]
+        ood_ipt = ood_ipt[indices_ood][:20000]
+        ood_opt = ood_opt[indices_ood][:20000]
+
+        eval = get_uncertainty_evaluator('wasserstein')
+        results = eval.evaluate(model, (id_ipt, id_opt), (ood_ipt, ood_opt))
+
+        print(results)
+        
+        id_ue = results['id_ue']
+        ood_ue = results['ood_ue']
 
         fig, ax = plt.subplots()
-        ax.ecdf(id_ue.flatten(), label='ID')
-        ax.ecdf(ood_ue.flatten(), label='OOD')
+        ax.ecdf(id_ue.data.flatten(), label='ID')
+        ax.ecdf(ood_ue.data.flatten(), label='OOD')
         ax.legend()
         # save it to png file
         plt.savefig('uncertainty.png')
+        plt.clf()
+        fig, ax = plt.subplots()
+
+        ax.ecdf(id_ue.data[1].flatten(), label='ID')
+        ax.ecdf(ood_ue.data[1].flatten(), label='OOD')
+        ax.legend()
+        # save it to png file
+        plt.savefig('uncertainty_1.png')
+
 
 
 
